4171e327e2cea9095063f924757211856a35aaaa
Abhishek Jee <abhishek.jee.2411@gmail.com>
2018-01-23 17:51:09 +0530

    Committing

--- mysql_commands.sql
+++ mysql_commands.sql
@@ -2,6 +2,10 @@
 
 select database();
 
+--To check which user you have logged in as:
+
+select user();
+
 /*
 If you have not selected any database, then the result is NULL.
 If you have, the result is the name of the database you are connectred to currently.
@@ -33,4 +37,42 @@ show index from <tablename>;
 
 insert into user (host, user, password, select_priv, insert_priv, update_priv)
 values ('localhost','guest',password('guest123'),'Y','Y','Y');
+
+--The following command reloads the grant tables, thereafter enabling the newly created user to log in to the database:
+
+flush privileges;
+
+--Following are a list of privileges that can be set to Y/N for every user's entry in MySQL:
+
+select_priv
+insert_priv
+update_priv
+delete_priv
+create_priv
+drop_priv
+reload_priv
+shutdown_priv
+process_priv
+file_priv
+grant_priv
+references_priv
+index_priv
+alter_priv
+
+--Using GRANT SELECT to add a new user:
+
+grant select, insert, update, delete, create, drop on alpha.* to 'zara'@'localhost' identified by 'zara123';
+
+--Admin commands:
+
+use <databasename>;
+show databases;
+show tables;
+show columns from <tablename>;
+show index from <tablename>;
+show table status like <tablename>\G;
+
+----------------------------
+--	MYSQL CONNECTION
+----------------------------
 
