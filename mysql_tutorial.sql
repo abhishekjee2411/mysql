9d101578a05d437949bfb93740d82c3374ada521
Abhishek Jee <abhishek.jee.2411@gmail.com>
2018-02-01 19:04:53 +0530

    commit

--- mysql_tutorial.sql
+++ mysql_tutorial.sql
@@ -80,4 +80,119 @@ AUTOCOMMIT
 "	
 show global variables like 'autocommit';
 
+--TRANSACTION-SAFE TABLE TYPES IN MYSQL
+
+InnoDB
+	It is a type of database that supports transactions.
+
+-------------------------------------------------------------------------------------------------------------------------------------------------
+-------------------------
+--REFERENTIAL INTEGRITY--
+-------------------------
+
+create table demotab
+(
+slno int unique
+);
+
+insert into demotab values (1);
+insert into demotab values (2);
+insert into demotab values (3);
+insert into demotab values (30);
+insert into demotab values (4);
+insert into demotab values (NULL);
+insert into demotab values (5);
+insert into demotab values (6);
+insert into demotab values (NULL);
+insert into demotab values (60);
+insert into demotab values (7);
+insert into demotab values (80);
+insert into demotab values (800);
+insert into demotab values (9);
+insert into demotab values (NULL);
+
+--Unique key allows NULL values.
+
+delete from demotab;
+
+insert ignore into demotab values (1);
+insert ignore into demotab values (2);
+insert ignore into demotab values (3);
+insert ignore into demotab values (3);
+insert ignore into demotab values (4);
+insert ignore into demotab values (NULL);
+insert ignore into demotab values (5);
+insert ignore into demotab values (6);
+insert ignore into demotab values (NULL);
+insert ignore into demotab values (6);
+insert ignore into demotab values (7);
+insert ignore into demotab values (8);
+insert ignore into demotab values (8);
+insert ignore into demotab values (9);
+insert ignore into demotab values (NULL);
+
+--INSERT IGNORE will silently discard (without generating an error) the insert if that insert causes duplicate values to be inserted into the table.
+
+delete from demotab;
+
+replace into demotab values (1);
+replace into demotab values (2);
+replace into demotab values (3);
+replace into demotab values (3);
+replace into demotab values (4);
+replace into demotab values (NULL);
+replace into demotab values (5);
+replace into demotab values (6);
+replace into demotab values (NULL);
+replace into demotab values (6);
+replace into demotab values (7);
+replace into demotab values (8);
+replace into demotab values (8);
+replace into demotab values (9);
+replace into demotab values (NULL);
+
+--REPLACE will replace the existing value with its duplicate without generating an error.
+
+--IFNULL() to substitute the NULL values with another tangible value, say 0 :
+
+select ifnull(slno,0) from demotab;
+
+--ISNULL() will return 1 if the value is NULL, 0 otherwise
+
+select isnull(slno) from demotab;
+
+--PRIMARY KEY--
+
+create table demotab2
+(
+	slno int primary key
+);
+
+insert into demotab2 values (1);
+insert into demotab2 values (null);
+insert into demotab2 values (2);
+insert into demotab2 values (null);
+
+--Primary key does not permit NULL values.
+
+create table demotab3
+(
+	slno int
+);
+
+insert into demotab3 values (1);
+insert into demotab3 values (2);
+insert into demotab3 values (3);
+insert into demotab3 values (3);
+insert into demotab3 values (4);
+insert into demotab3 values (NULL);
+insert into demotab3 values (5);
+insert into demotab3 values (6);
+insert into demotab3 values (NULL);
+insert into demotab3 values (6);
+insert into demotab3 values (7);
+insert into demotab3 values (8);
+insert into demotab3 values (8);
+insert into demotab3 values (9);
+insert into demotab3 values (NULL);
 
